c6be0492fb1f2d71d902769569ec0c9d0a607c91
nash403 <honore.nintunze@gmail.com>
2015-11-29 23:06:20 +0100

    main

---
+++ src/TestPizza.java
@@ -0,0 +1,48 @@
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class TestPizza {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1)
+			System.out.println("java TestPizza  file");
+		else {
+			Scanner donnee = new Scanner(new FileReader(args[0]));
+			System.out.println("Fichier lu:");
+			int l = donnee.nextInt();
+			System.out.print(l + " ");
+			int h = donnee.nextInt();
+			System.out.print(h + " ");
+			int n = donnee.nextInt();
+			System.out.print(n + " ");
+			int c = donnee.nextInt();
+			System.out.println(c + " ");
+			Garniture lapizza[][] = new Garniture[l][h];
+
+			for (int i = 0; i < l; i++) {
+				String line = donnee.next();
+				for (int j = 0; j < h; j++) {
+					char g  = line.charAt(j);
+					//System.out.print(g);
+					switch (g) {
+					case 'T':
+						lapizza[i][j] = Garniture.TOMATE;
+						break;
+					case 'H':
+						lapizza[i][j] = Garniture.JAMBON;
+						break;
+
+					default:
+						throw new Exception("bad entry");
+					}
+				}
+				//System.out.println();
+			}
+			donnee.close();
+			Pizza pizza = new Pizza(lapizza, c, n);
+			System.out.println(pizza.all().size());
+		}
+
+	}
+
+}
